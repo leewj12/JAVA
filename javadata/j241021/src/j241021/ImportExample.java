9aa003898d716800eb32481f0025644e2ed6a9e6
leewj12 <pyth0n9q@gmail.com>
2024-10-21 18:49:10 +0900

    Add files via upload

---
+++ javadata/j241021/src/j241021/ImportExample.java
@@ -0,0 +1,16 @@
+package j241021;
+
+import java.util.*;
+import java.util.concurrent.*;
+
+public class ImportExample {
+
+	public static void main(String[] args) {
+		
+		System.out.println("당신의 이름을 입력하세요");
+		Scanner sc = new Scanner(System.in);
+		String name = sc.next();
+		
+	}
+
+}
